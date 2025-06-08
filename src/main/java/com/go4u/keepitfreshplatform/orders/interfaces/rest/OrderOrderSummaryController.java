5bf5d8e23aaa05e58759637c49bbdf514dc6cace
RominaMaita <u202213765@upc.edu.pe>
2025-06-08 12:59:22 -0500

    feat(orders): add order controllers.

---
+++ src/main/java/com/go4u/keepitfreshplatform/orders/interfaces/rest/OrderOrderSummaryController.java
@@ -0,0 +1,63 @@
+package com.go4u.keepitfreshplatform.orders.interfaces.rest;
+
+import com.go4u.keepitfreshplatform.orders.domain.model.commands.AddDishToOrderCommand;
+import com.go4u.keepitfreshplatform.orders.domain.model.queries.GetOrderByIdQuery;
+import com.go4u.keepitfreshplatform.orders.domain.model.valueobjects.Quantity;
+import com.go4u.keepitfreshplatform.orders.domain.services.OrderCommandService;
+import com.go4u.keepitfreshplatform.orders.domain.services.OrderQueryService;
+import com.go4u.keepitfreshplatform.orders.interfaces.rest.resources.AddOrderItemResource;
+import com.go4u.keepitfreshplatform.orders.interfaces.rest.resources.OrderItemResource;
+import com.go4u.keepitfreshplatform.orders.interfaces.rest.transform.OrderItemResourceFromEntityAssembler;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+
+@RestController
+@RequestMapping(value = "/orders/{orderId}/order-items",produces = APPLICATION_JSON_VALUE)
+@Tag(name = "Orders")
+public class OrderOrderSummaryController {
+
+    private final OrderCommandService orderCommandService;
+    private final OrderQueryService orderQueryService;
+
+    public OrderOrderSummaryController(OrderCommandService orderCommandService, OrderQueryService orderQueryService) {
+        this.orderCommandService = orderCommandService;
+        this.orderQueryService = orderQueryService;
+    }
+
+    /**
+     * Adds an order item to order summary
+     */
+    @PostMapping
+    public ResponseEntity<OrderItemResource> addOrderItemToOrderSummary(
+            @PathVariable Long orderId, @RequestBody AddOrderItemResource resource) {
+
+        var addDishToOrderCommand = new AddDishToOrderCommand(orderId, resource.dishId(), new Quantity(resource.quantity()));
+
+        try{
+            orderCommandService.handle(addDishToOrderCommand);
+
+            var updatedOrder = orderQueryService.handle(new GetOrderByIdQuery(orderId));
+            if(updatedOrder.isEmpty()) return ResponseEntity.notFound().build();
+
+            var order = updatedOrder.get();
+
+            var items = order.getOrderSummary().getOrderItems();
+            if(items.isEmpty()) return ResponseEntity.internalServerError().build();
+
+            var lastItem = items.getLast();
+
+            var itemResource = OrderItemResourceFromEntityAssembler.toResourceFromEntity(lastItem);
+
+            return new ResponseEntity<>(itemResource, HttpStatus.CREATED);
+
+        }catch (IllegalArgumentException e) {
+            return ResponseEntity.badRequest().body(null);
+        }
+    }
+
+
+}
---
+++ src/main/java/com/go4u/keepitfreshplatform/orders/interfaces/rest/OrdersController.java
@@ -0,0 +1,72 @@
+package com.go4u.keepitfreshplatform.orders.interfaces.rest;
+
+
+import com.go4u.keepitfreshplatform.orders.domain.model.queries.GetAllOrdersQuery;
+import com.go4u.keepitfreshplatform.orders.domain.model.queries.GetOrderByIdQuery;
+import com.go4u.keepitfreshplatform.orders.domain.model.queries.GetOrdersByTableNumberQuery;
+import com.go4u.keepitfreshplatform.orders.domain.services.OrderCommandService;
+import com.go4u.keepitfreshplatform.orders.domain.services.OrderQueryService;
+import com.go4u.keepitfreshplatform.orders.interfaces.rest.resources.CreateOrderResource;
+import com.go4u.keepitfreshplatform.orders.interfaces.rest.resources.OrderResource;
+import com.go4u.keepitfreshplatform.orders.interfaces.rest.transform.CreateOrderCommandFromResourceAssembler;
+import com.go4u.keepitfreshplatform.orders.interfaces.rest.transform.OrderResourceFromEntityAssembler;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+
+@RestController
+@RequestMapping(value = "/api/v1/orders", produces = APPLICATION_JSON_VALUE)
+@Tag(name = "Orders", description = "Order Management Endpoints")
+public class OrdersController {
+
+    private final OrderCommandService orderCommandService;
+    private final OrderQueryService orderQueryService;
+
+    public OrdersController(OrderCommandService orderCommandService, OrderQueryService orderQueryService) {
+        this.orderCommandService = orderCommandService;
+        this.orderQueryService = orderQueryService;
+    }
+
+    @PostMapping
+    public ResponseEntity<OrderResource> createOrder(@RequestBody CreateOrderResource createOrderResource) {
+        var createOrderCommand = CreateOrderCommandFromResourceAssembler.toCommandFromResource(createOrderResource);
+        var orderId = orderCommandService.handle(createOrderCommand);
+        if (orderId == null) {
+            return ResponseEntity.badRequest().build();
+        }
+        var getOrderByIdQuery = new GetOrderByIdQuery(orderId);
+        var order = orderQueryService.handle(getOrderByIdQuery);
+        if (order.isEmpty()) return ResponseEntity.badRequest().build();
+        var orderResource = OrderResourceFromEntityAssembler.toResourceFromEntity(order.get());
+        return new ResponseEntity<>(orderResource, HttpStatus.CREATED);
+    }
+
+
+    @GetMapping("/table/{tableNumber}")
+    public ResponseEntity<List<OrderResource>> getOrderByTableNumber(@PathVariable int tableNumber) {
+        var getOrderByTableNumberQuery = new GetOrdersByTableNumberQuery(tableNumber);
+        var orders = orderQueryService.handle(getOrderByTableNumberQuery);
+        if (orders.isEmpty()) return ResponseEntity.badRequest().build();
+        var resources = orders.stream()
+                .map(OrderResourceFromEntityAssembler::toResourceFromEntity)
+                .toList();
+        return ResponseEntity.ok(resources);
+
+    }
+
+    @GetMapping
+    public ResponseEntity<List<OrderResource>> getAllOrders() {
+        var getAllOrdersQuery = new GetAllOrdersQuery();
+        var orders = orderQueryService.handle(getAllOrdersQuery);
+        var orderResources = orders.stream().map(OrderResourceFromEntityAssembler::toResourceFromEntity).toList();
+        return ResponseEntity.ok(orderResources);
+    }
+
+
+}
