4af56c603fa093d042d0f45661c4479842c9e574
RominaMaita <u202213765@upc.edu.pe>
2025-06-07 23:35:33 -0500

    feat(orders): add order and dish commands.

---
+++ src/main/java/com/go4u/keepitfreshplatform/orders/domain/model/commands/AddDishToOrderCommand.java
@@ -0,0 +1,8 @@
+package com.go4u.keepitfreshplatform.orders.domain.model.commands;
+
+import com.go4u.keepitfreshplatform.orders.domain.model.valueobjects.Quantity;
+
+import java.math.BigDecimal;
+
+public record AddDishToOrderCommand(Long orderId, Long dishId, Quantity quantity, BigDecimal price) {
+}
