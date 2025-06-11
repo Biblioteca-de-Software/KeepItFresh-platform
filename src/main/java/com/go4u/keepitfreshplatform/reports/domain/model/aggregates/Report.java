f0902e661d8148cfea732a74a17021b7894e92ad
Werner Lang <u202310003@upc.edu.pe>
2025-06-10 21:14:29 -0500

    chore: initial commit

---
+++ src/main/java/com/go4u/keepitfreshplatform/reports/domain/model/aggregates/Report.java
@@ -0,0 +1,12 @@
+package com.go4u.keepitfreshplatform.reports.domain.model.aggregates;
+
+import com.go4u.keepitfreshplatform.shared.domain.model.aggregates.AuditableAbstractAggregateRoot;
+import jakarta.persistence.Entity;
+import lombok.Getter;
+
+@Getter
+@Entity
+public class Report extends AuditableAbstractAggregateRoot<Report> {
+    private String title;
+    private String description;
+}
