4ee082221dda454e3eff46526c11394345d059d5
Werner Lang <U202310003@upc.edu.pe>
2025-06-11 09:28:37 -0500

    fix: add missing param on Report aggregate.

--- src/main/java/com/go4u/keepitfreshplatform/reports/domain/model/aggregates/Report.java
+++ src/main/java/com/go4u/keepitfreshplatform/reports/domain/model/aggregates/Report.java
@@ -1,12 +1,18 @@
 package com.go4u.keepitfreshplatform.reports.domain.model.aggregates;
 
+import com.go4u.keepitfreshplatform.reports.domain.model.valueobjects.ReportId;
 import com.go4u.keepitfreshplatform.shared.domain.model.aggregates.AuditableAbstractAggregateRoot;
+import jakarta.persistence.Embedded;
 import jakarta.persistence.Entity;
 import lombok.Getter;
 
 @Getter
 @Entity
 public class Report extends AuditableAbstractAggregateRoot<Report> {
+
     private String title;
     private String details;
+
+    @Embedded
+    private ReportId reportId;
 }
