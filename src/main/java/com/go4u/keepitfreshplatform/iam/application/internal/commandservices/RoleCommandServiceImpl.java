6b11d412bf39707ff8062f10044bf605ed8836c4
gabriel <u202220659@upc.edu.pe>
2025-06-13 17:45:14 -0500

    feat: add RoleCommandsService

---
+++ src/main/java/com/go4u/keepitfreshplatform/iam/application/internal/commandservices/RoleCommandServiceImpl.java
@@ -0,0 +1,31 @@
+package com.go4u.keepitfreshplatform.iam.application.internal.commandservices;
+import com.go4u.keepitfreshplatform.iam.domain.model.entities.Role;
+import com.go4u.keepitfreshplatform.iam.domain.model.valueobjects.Roles;
+import com.go4u.keepitfreshplatform.iam.domain.services.RoleCommandsService;
+import com.go4u.keepitfreshplatform.iam.domain.model.commands.SeedRolesCommand;
+
+import com.go4u.keepitfreshplatform.iam.infraestructure.persistence.jpa.repository.RoleRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.Arrays;
+@Service
+public  class RoleCommandServiceImpl implements RoleCommandsService {
+    private final RoleRepository roleRepository;
+
+    public RoleCommandServiceImpl(RoleRepository roleRepository) {
+        this.roleRepository = roleRepository;
+    }
+
+    // inherit javadoc
+    @Override
+    public void handle(SeedRolesCommand command) {
+        Arrays.stream(Roles.values()).forEach(role -> {
+            if (!roleRepository.existsByName(role)) {
+                roleRepository.save(new Role(Roles.valueOf(role.name())));
+            }
+        });
+    }
+}
+
+
+
