32760be876496254322f3bf0e45706dc17af10c8
Paolo <u20221f613@upc.edu.pe>
2025-06-14 00:33:57 -0500

    feat(inventory): fix CreateProductCommand attributes.

--- src/main/java/com/go4u/keepitfreshplatform/inventory/domain/model/commands/CreateProductCommand.java
+++ src/main/java/com/go4u/keepitfreshplatform/inventory/domain/model/commands/CreateProductCommand.java
@@ -1,8 +1,4 @@
 package com.go4u.keepitfreshplatform.inventory.domain.model.commands;
 
-import com.go4u.keepitfreshplatform.inventory.domain.model.valueobjects.ExpirationDate;
-import com.go4u.keepitfreshplatform.inventory.domain.model.valueobjects.Price;
-import com.go4u.keepitfreshplatform.inventory.domain.model.valueobjects.Quantity;
-
 public record CreateProductCommand(String name) {
 }
