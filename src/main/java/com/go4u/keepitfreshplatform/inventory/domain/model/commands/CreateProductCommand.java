7846d52a7b9a444ca5f95a54bd3f4e676445a954
Paolo <u20221f613@upc.edu.pe>
2025-06-14 14:23:45 -0500

    feat(inventory): fix creation command

--- src/main/java/com/go4u/keepitfreshplatform/inventory/domain/model/commands/CreateProductCommand.java
+++ src/main/java/com/go4u/keepitfreshplatform/inventory/domain/model/commands/CreateProductCommand.java
@@ -1,5 +1,12 @@
 package com.go4u.keepitfreshplatform.inventory.domain.model.commands;
 
-public record CreateProductCommand(Long productItemId) {
+import java.math.BigDecimal;
+import java.time.LocalDate;
 
-}
+public record CreateProductCommand(
+        String name,
+        Long productId,
+        Integer quantity,
+        LocalDate expirationDate,
+        BigDecimal price
+) {}
