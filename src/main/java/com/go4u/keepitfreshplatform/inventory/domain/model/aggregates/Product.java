c59f877133722e7a4592efc54cd13c0dee1d8d87
Paolo <u20221f613@upc.edu.pe>
2025-06-14 14:24:23 -0500

    feat(inventory): fix aggregate

--- src/main/java/com/go4u/keepitfreshplatform/inventory/domain/model/aggregates/Product.java
+++ src/main/java/com/go4u/keepitfreshplatform/inventory/domain/model/aggregates/Product.java
@@ -1,29 +1,43 @@
 package com.go4u.keepitfreshplatform.inventory.domain.model.aggregates;
 
 import com.go4u.keepitfreshplatform.inventory.domain.model.commands.CreateProductCommand;
+import com.go4u.keepitfreshplatform.inventory.domain.model.valueobjects.ExpirationDate;
+import com.go4u.keepitfreshplatform.inventory.domain.model.valueobjects.Price;
+import com.go4u.keepitfreshplatform.inventory.domain.model.valueobjects.ProductId;
+import com.go4u.keepitfreshplatform.inventory.domain.model.valueobjects.Quantity;
 import com.go4u.keepitfreshplatform.shared.domain.model.aggregates.AuditableAbstractAggregateRoot;
+import jakarta.persistence.Embedded;
 import jakarta.persistence.Entity;
 import lombok.Getter;
 import org.apache.logging.log4j.util.Strings;
 
-@Getter
 @Entity
+@Getter
 public class Product extends AuditableAbstractAggregateRoot<Product> {
 
     private String name;
+
+    @Embedded
+    private ProductId productId;
+
+    @Embedded
+    private Quantity quantity;
+
+    @Embedded
+    private ExpirationDate expirationDate;
+
+    @Embedded
+    private Price price;
 
     public Product() {
         this.name = Strings.EMPTY;
     }
 
-    public Product(String name) {
-        this.name = name;
-    }
-
     public Product(CreateProductCommand command) {
         this.name = command.name();
+        this.productId = new ProductId(command.productId());
+        this.quantity = new Quantity(command.quantity());
+        this.expirationDate = new ExpirationDate(command.expirationDate());
+        this.price = new Price(command.price());
     }
-
-
-
 }
