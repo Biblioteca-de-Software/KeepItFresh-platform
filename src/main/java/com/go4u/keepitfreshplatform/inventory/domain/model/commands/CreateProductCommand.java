35509fd6a381850972003b0dddd6b43954eaa002
Paolo <u20221f613@upc.edu.pe>
2025-06-14 00:21:03 -0500

    feat(inventory): change productId variable type.

--- src/main/java/com/go4u/keepitfreshplatform/inventory/domain/model/commands/CreateProductCommand.java
+++ src/main/java/com/go4u/keepitfreshplatform/inventory/domain/model/commands/CreateProductCommand.java
@@ -2,10 +2,9 @@ package com.go4u.keepitfreshplatform.inventory.domain.model.commands;
 
 import com.go4u.keepitfreshplatform.inventory.domain.model.valueobjects.ExpirationDate;
 import com.go4u.keepitfreshplatform.inventory.domain.model.valueobjects.Price;
-import com.go4u.keepitfreshplatform.inventory.domain.model.valueobjects.ProductId;
 import com.go4u.keepitfreshplatform.inventory.domain.model.valueobjects.Quantity;
 
-public record CreateProductCommand(ProductId productId,
+public record CreateProductCommand(Long productId,
                                    String name,
                                    Price price,
                                    Quantity quantity,
