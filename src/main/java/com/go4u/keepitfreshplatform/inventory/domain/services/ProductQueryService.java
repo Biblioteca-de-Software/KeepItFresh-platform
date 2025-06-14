e3e587cd9261a2b8487aad149b82d37e91ec2c55
Paolo <u20221f613@upc.edu.pe>
2025-06-14 14:06:21 -0500

    feat(inventory): fix ProductQueryService

--- src/main/java/com/go4u/keepitfreshplatform/inventory/domain/services/ProductQueryService.java
+++ src/main/java/com/go4u/keepitfreshplatform/inventory/domain/services/ProductQueryService.java
@@ -2,9 +2,12 @@ package com.go4u.keepitfreshplatform.inventory.domain.services;
 
 import com.go4u.keepitfreshplatform.inventory.domain.model.aggregates.Product;
 import com.go4u.keepitfreshplatform.inventory.domain.model.queries.GetAllProductsQuery;
+import com.go4u.keepitfreshplatform.inventory.domain.model.queries.GetProductByIdQuery;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface ProductQueryService {
     List<Product> handle(GetAllProductsQuery query);
+    Optional<Product> handle(GetProductByIdQuery query);
 }
