2787ed64e2875471d09e9433f52e4aa16d318df9
Paolo <u20221f613@upc.edu.pe>
2025-06-14 13:13:22 -0500

    feat(inventory): fix ProductItemResource

--- src/main/java/com/go4u/keepitfreshplatform/inventory/interfaces/rest/resources/ProductItemResource.java
+++ src/main/java/com/go4u/keepitfreshplatform/inventory/interfaces/rest/resources/ProductItemResource.java
@@ -5,7 +5,7 @@ import com.go4u.keepitfreshplatform.inventory.domain.model.valueobjects.Price;
 import com.go4u.keepitfreshplatform.inventory.domain.model.valueobjects.Quantity;
 
 public record ProductItemResource(Long productItemId,
-                                  String productName,
+                                  Long productName,
                                   Quantity quantity,
                                   ExpirationDate expirationDate,
                                   Price price) {
