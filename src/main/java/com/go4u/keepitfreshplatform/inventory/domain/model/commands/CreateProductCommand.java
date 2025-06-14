d6962a455aad636ef9416b116a299454f405affb
Paolo <u20221f613@upc.edu.pe>
2025-06-14 00:28:44 -0500

    feat(inventory): fix productId attributes.

--- src/main/java/com/go4u/keepitfreshplatform/inventory/domain/model/commands/CreateProductCommand.java
+++ src/main/java/com/go4u/keepitfreshplatform/inventory/domain/model/commands/CreateProductCommand.java
@@ -4,8 +4,7 @@ import com.go4u.keepitfreshplatform.inventory.domain.model.valueobjects.Expirati
 import com.go4u.keepitfreshplatform.inventory.domain.model.valueobjects.Price;
 import com.go4u.keepitfreshplatform.inventory.domain.model.valueobjects.Quantity;
 
-public record CreateProductCommand(Long productId,
-                                   String name,
+public record CreateProductCommand(String name,
                                    Price price,
                                    Quantity quantity,
                                    ExpirationDate expirationDate) {
