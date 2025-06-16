3982b77094d450942e87cb3ea620c8e3f121513e
Werner Lang <u202310003@upc.edu.pe>
2025-06-15 19:56:22 -0500

    feat(reports): add ReportQueryServiceImpl

---
+++ src/main/java/com/go4u/keepitfreshplatform/reports/application/internal/queryservices/ReportQueryServiceImpl.java
@@ -0,0 +1,29 @@
+package com.go4u.keepitfreshplatform.reports.application.internal.queryservices;
+
+import com.go4u.keepitfreshplatform.reports.domain.model.aggregates.Report;
+import com.go4u.keepitfreshplatform.reports.domain.model.queries.GetAllReportsQuery;
+import com.go4u.keepitfreshplatform.reports.domain.model.queries.GetReportByIdQuery;
+import com.go4u.keepitfreshplatform.reports.domain.services.ReportQueryService;
+import com.go4u.keepitfreshplatform.reports.infrastructure.persistence.jpa.repositories.ReportRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public class ReportQueryServiceImpl implements ReportQueryService {
+
+    private final ReportRepository reportRepository;
+
+    public ReportQueryServiceImpl(ReportRepository reportRepository) {
+        this.reportRepository = reportRepository;
+    }
+
+    @Override
+    public List<Report> handle(GetAllReportsQuery query) {
+        return reportRepository.findAll();
+    }
+
+    @Override
+    public Optional<Report> handle(GetReportByIdQuery query) {
+        return reportRepository.findById(query.id());
+    }
+}
