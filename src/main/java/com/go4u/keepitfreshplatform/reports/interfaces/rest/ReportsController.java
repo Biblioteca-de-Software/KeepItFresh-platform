4dffe084cf409e6edc12b7dda60b5489981fda7d
Werner Lang <u202310003@upc.edu.pe>
2025-06-16 14:11:21 -0500

    fix: Incorrect RequestBody usage

--- src/main/java/com/go4u/keepitfreshplatform/reports/interfaces/rest/ReportsController.java
+++ src/main/java/com/go4u/keepitfreshplatform/reports/interfaces/rest/ReportsController.java
@@ -11,15 +11,11 @@ import com.go4u.keepitfreshplatform.reports.interfaces.rest.resources.ReportReso
 import com.go4u.keepitfreshplatform.reports.interfaces.rest.transform.CreateReportCommandFromResourceAssembler;
 import com.go4u.keepitfreshplatform.reports.interfaces.rest.transform.ReportResourceFromEntityAssembler;
 import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.parameters.RequestBody;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
@@ -48,7 +44,7 @@ public class ReportsController {
     })
     @PostMapping()
     public ResponseEntity<ReportResource> createReport(
-            @RequestBody(description = "Details of the report to be created") CreateReportResource createReportResource) {
+            @RequestBody CreateReportResource createReportResource) {
         var createReportCommand = CreateReportCommandFromResourceAssembler.toCommandFromResource(createReportResource);
         Optional<Report> report = reportCommandService.handle(createReportCommand);
         if(report.isEmpty()){
