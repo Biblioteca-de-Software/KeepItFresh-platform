f5fb3a71cf2637d9f18e5870832b53e288052e6c
RominaMaita <u202213765@upc.edu.pe>
2025-06-18 20:30:27 -0500

    chore: update comments in pom.xml for clarity and consistency

--- src/main/java/com/go4u/keepitfreshplatform/iam/application/internal/outboundservices/hashing/HashingService.java
+++
@@ -1,9 +0,0 @@
-package com.go4u.keepitfreshplatform.iam.application.internal.outboundservices.hashing;
-
-
-public interface HashingService {
-
-    String encode(CharSequence rawPassword);
-
-    boolean matches(CharSequence rawPassword, String encodedPassword);
-}
--- src/main/java/com/go4u/keepitfreshplatform/iam/interfaces/rest/resources/UserResource.java
+++
@@ -1,5 +0,0 @@
-package com.go4u.keepitfreshplatform.iam.interfaces.rest.resources;
-import java.util.List;
-
-public record UserResource(Long id, String username, List<String> roles) {
-}
